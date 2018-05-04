840f6794956c24840bf952353675644d769bea17
Jesse Peplinski <peplinski.jesse@gmail.com>
2018-05-04 13:01:14 -0400

    Click to toggle gamestats

--- src/Components/Match/Match.js
+++ src/Components/Match/Match.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import PUBG from '../PUBG/PUBG';
 
-class Match extends React.Component {    
+class Match extends React.Component {
     constructor(props) {
         super(props);
 
@@ -11,11 +11,12 @@ class Match extends React.Component {
             assists: '',
             damageDealt: '',
             headshotKills: '',
-            winPlace: ''
+            winPlace: '',
+            showing: false
         };
 
         this.searchMatch = this.searchMatch.bind(this);
-    }   
+    }
 
     // TODO: Pass in username here as well
     searchMatch() {
@@ -33,21 +34,25 @@ class Match extends React.Component {
     // TODO: Abstract this into MatchInformation component
     // TODO: Add logic for rendering fields better (should be able to get this logic from jamming for + and -)
     render() {
-        return(
+        const { showing } = this.state;
+        return (
             <div className="Match" key={this.props.match.id}>
                 <div className="Match-information">
                     <h3>{this.props.match.id}</h3>
-                    <a onClick={this.searchMatch}>View Match Results</a>
-
                     <p>MORE META DATA SHOULD GO HERE.</p>
-
-                    <p>Kills: {this.state.kills}</p>
-                    <p>Assists: {this.state.assists}</p>
-                    <p>damageDealt: {this.state.damageDealt}</p>
-                    <p>headshotKills: {this.state.headshotKills}</p>
-                    <p>winPlace: {this.state.winPlace}</p>
-                    
-
+                    <button onClick={() => {this.setState({ showing: !showing }); this.searchMatch();}}>View Match Results</button>
+                    {/* Toggle game stats onClick */}
+                    { showing 
+                        ? 
+                        <div className="gamestats">
+                            <p>Kills: {this.state.kills}</p>
+                            <p>Assists: {this.state.assists}</p>
+                            <p>damageDealt: {this.state.damageDealt}</p>
+                            <p>headshotKills: {this.state.headshotKills}</p>
+                            <p>winPlace: {this.state.winPlace}</p>
+                        </div>
+                        : null
+                    }
                 </div>
             </div>
         );
